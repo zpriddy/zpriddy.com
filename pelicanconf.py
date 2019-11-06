b57199705f6c4682f74d46ddfd131b4f63d5846f
Zachary Priddy <github@zpriddy.com>
2019-11-05 16:07:33 -0800

    Try Again

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Zachary Priddy'
 SITENAME = 'zpriddy.com'
-SITEURL = ''
+SITEURL = 'https://zpriddy.github.io/zpriddy.com/output/'
 
 PATH = 'content'
 
