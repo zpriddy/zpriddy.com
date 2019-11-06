f35d7aaa4a07f48cf2f3eb09947eaf99391b416c
Zachary Priddy <github@zpriddy.com>
2019-11-05 17:11:17 -0800

    test theme

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://zpriddy.github.io/zpriddy.com'
+SITEURL = 'https://zpriddy.com'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
